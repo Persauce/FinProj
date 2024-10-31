49d5c9a15c885b79aca85502e11f2f81d1c4ec01
Dave Persaud <davep218@outlook.com>
2024-10-31 10:26:32 -0400

    yeg werdna

--- email.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $POST['message'];
-$subscribe = isset($_POST['subscribe']) ? $_POST['subscribe'] : '';
-echo "<h1>name:</h1> " . htmlspecialchars($name);
-?>
